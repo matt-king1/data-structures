b0c4712a4634caf1100b2afb74ca5d9d1b322105
matt-king1 <maki2275@colorado.edu>
2020-01-30 18:41:11 -0700

    Assignment 2 at 4/6

--- Assignment-2/main.cpp
+++ Assignment-2/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 
 using namespace std;
+
 struct wordItem
 {
     string word;
@@ -11,11 +12,12 @@ struct wordItem
 void getStopWords(const char* ignoreWordFileName, string ignoreWords[])
 {
     ifstream ignoreIFS(ignoreWordFileName);
-    if(!ignoreIFS.is_open)
+
+    if(!ignoreIFS.is_open())
     {
         cout << "Failed to open " << ignoreWordFileName << endl;
     }
-    
+
     string word;
     int index = 0;
     while (ignoreIFS >> word )
@@ -26,18 +28,64 @@ void getStopWords(const char* ignoreWordFileName, string ignoreWords[])
     ignoreIFS.close(); // close the file
 }
 
+bool isStopWord(string word, string ignoreWords[])
+{
+    for(int i = 0; i < 50; i++)
+    {
+        if(ignoreWords[i] == word)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int getTotalNumberNonStopWords(wordItem uniqueWords[], int length)
+{
+    int count = 0;
+    for(int i = 0; i < length; i++)
+    {
+        count += uniqueWords[i].count;
+    }
+    return count;
+}
+
+void arraySort(wordItem uniqueWords[], int length)
+{
+    wordItem temp;
+    for(int i = 0; i < length-1; i++)
+    {
+        for(int j = 0; j < length-i-1; j++)
+        {
+            if(uniqueWords[j].count < uniqueWords[j+1].count)
+            {
+                wordItem temp = uniqueWords[j];
+                uniqueWords[j] = uniqueWords[j+1];
+                uniqueWords[j+1] = temp;
+            }
+        }
+    }
+}
+
+void printNext10(wordItem uniqueWords[], int N, int totalNumWords)
+{
+    for(int i = N; i < N+10; i++)
+    {
+
+    }
+}
+
 int main(int argc, char* argv[])
 {
     
-if(argc != 4) 
-{
-    cout << "Usage: Assignment2Solution <number of words> <inputfilename.txt> <ignoreWordsfilename.txt>" << endl;
-    return 1;
-}
+    if(argc != 4) 
+    {
+        cout << "Usage: Assignment2Solution <number of words> <inputfilename.txt> <ignoreWordsfilename.txt>" << endl;
+        return 1;
+    }
 
-int N = stoi(argv[1]); //N
-string textFile = argv[2]; //TomSawyer.txt
-string ignoreFile = argv[3]; //ignoreWords.txt
-
+    int N = stoi(argv[1]); //N
+    string textFile = argv[2]; //TomSawyer.txt
+    string ignoreFile = argv[3]; //ignoreWords.txt
 
 }
