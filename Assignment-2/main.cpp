068f8934ae183c480b4229adb667f418049ff653
matt-king1 <maki2275@colorado.edu>
2020-02-02 19:45:01 -0700

    Assignment 2 5/6

--- Assignment-2/main.cpp
+++ Assignment-2/main.cpp
@@ -75,6 +75,42 @@ void printNext10(wordItem uniqueWords[], int N, int totalNumWords)
     }
 }
 
+wordItem* resize(wordItem* arrayPtr, int &capacity)
+{
+	//increase the capacity by two times
+	int newCapacity = 2 * capacity;
+    cout<<"Resizing from "<<capacity<<" to "<<newCapacity<<endl;
+
+	//dynamically allocate an array of size newCapacity
+	wordItem *newArray = new wordItem[newCapacity];
+
+	//copy all data from oldArray to newArray
+	for(int i = 0; i < capacity; i++)
+	{
+		newArray[i] = arrayPtr[i];
+	}
+
+	//free the memory associated with oldArray
+	delete[] arrayPtr;
+
+	capacity = newCapacity;
+
+	//return the newArray
+	return newArray;
+}
+
+bool isInUniqueWordsArr(string word, wordItem* uniqueWords, int len)
+{
+    for(int i = 0; i < len; i++)
+    {
+        if(uniqueWords[i].word == word)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
 int main(int argc, char* argv[])
 {  
     if(argc != 4) 
@@ -85,6 +121,56 @@ int main(int argc, char* argv[])
 
     int N = stoi(argv[1]); //N
     string textFile = argv[2]; //TomSawyer.txt
-    string ignoreFile = argv[3]; //ignoreWords.txt
+    string ignoreFile = argv[3]; //ignoreWords.txt  
 
+    string ignoreWords[50] = {};
+    getStopWords(ignoreFile.c_str(), ignoreWords);
+    
+    int capacity = 100;
+    wordItem* uniqueWords = new wordItem[capacity];
+
+    ifstream textFileIFS(textFile);
+    string word;
+    int index = 0;
+    int timesResized = 0;
+
+    while (textFileIFS >> word )
+    {
+        if(index == capacity-1)
+        {
+            resize(uniqueWords, capacity);
+            timesResized++;
+        }
+
+        cout << word << ": " << isStopWord(word, ignoreWords) << " " << isInUniqueWordsArr(word, uniqueWords, index) << endl;
+
+        if(!isStopWord(word, ignoreWords) && !isInUniqueWordsArr(word, uniqueWords, index))
+        {
+            cout << "Creating WI and adding to array at index: " << index << endl;
+            wordItem wi = {word, 1};
+            uniqueWords[index] = wi;
+            index++;
+            cout << "done" << endl;
+        } else if (!isStopWord(word, ignoreWords))
+        {
+            cout << "Looking for WI in array" << endl;
+            for(int i = 0; i < index; i++)
+            {
+                if(uniqueWords[i].word == word)
+                {
+                    cout << "Found word, incrementing count" << endl;
+                    uniqueWords[i].count++;
+                    //break;
+                }
+            }
+        }
+    }
+    cout << "DONE" << " " << index << endl;
+
+    for(int i = 0; i < index; i++)
+    {
+        cout << i << " " << uniqueWords[i].word << ": " << uniqueWords[i].count;
+    }
+
+    textFileIFS.close(); // close the file
 }
