256d460c254ba4e1dff96f81b67c1386835d9df6
matt-king1 <maki2275@colorado.edu>
2020-02-14 12:35:08 -0700

    Lab 5 complete

---
+++ Lab 5/Lab5/QueueLL.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include "QueueLL.hpp"
+
+using namespace std;
+
+QueueLL::QueueLL()
+{
+    queueFront = nullptr;
+    queueEnd = nullptr;
+}
+
+QueueLL::~QueueLL()
+{
+    while( !isEmpty() )
+        dequeue();
+}
+
+bool QueueLL::isEmpty()
+{
+    /*if(queueFront == nullptr || queueEnd == nullptr)
+        return true;
+    return false;*/
+    return (!queueFront || !queueEnd);
+}
+
+// TODO
+void QueueLL::enqueue(int key)
+{
+    Node *nn = new Node;
+    nn->key = key;
+    nn->next = nullptr;
+
+    if(isEmpty())
+    {
+        queueFront = nn;
+        queueEnd = nn;
+    } else {
+        queueEnd->next = nn;
+        queueEnd = nn;
+    }
+}
+
+//TODO
+void QueueLL::dequeue()
+{
+    if(!isEmpty())
+    {
+        if(queueFront->next == NULL)
+        {
+            delete queueFront;
+            queueFront = nullptr;
+            queueEnd = nullptr;
+        } else {
+            Node* tmp = queueFront;
+            queueFront = queueFront->next;
+            delete tmp;
+        }
+    } else {
+        cout<<"queue is empty. can not deque"<<endl;
+    }
+}
+
+int QueueLL::peek()
+{
+    if( !isEmpty() )
+        return queueFront->key;
+    else
+    {
+        cout<< " queue is empty. can not peek"<<endl;
+        return -1;
+    }
+    //return 0;
+}
