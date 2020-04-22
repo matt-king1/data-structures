36f8e03311529ec88e9e47b20390fb014f708c70
matt-king1 <maki2275@colorado.edu>
2020-04-22 13:09:49 -0600

    Added starter code

--- Final Project/main.cpp
+++ Final Project/main.cpp
@@ -10,4 +10,18 @@ int main(int argc, char** argv)
     string dataS;
 
     getline(dataAFile, dataS); // dataS now holds the entire contents of dataSetA.csv
+
+
+
+
+    // TIMER CODE GIVEN DURING LECTURE
+
+    /*
+
+    #include <chrono>
+    using namespace std::chrono;
+    high_resolution_clock::time_point start = high_resolution_clock::now(); high_resolution_clock::time_point end = high_resolution_clock::now();
+    duration<double> execTime = duration_cast<microseconds>(end - start); std::cout << "\nbubble execution time: " << execTime.count() << " seconds." <<  std::endl;
+
+    */
 }
